fd8dd8637e44fdc1cffd9f4aaaef815a4e2b4a46
Rafid <2018-025-318@student.cse.du.ac.bd>
2021-06-25 22:23:43 +0600

    UI changes

--- app/src/main/java/com/example/cashbook/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/cashbook/fragments/HistoryFragment.kt
@@ -7,9 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.cashbook.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -22,17 +24,22 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class HistoryFragment(activity:Activity) : Fragment() {
+class HistoryFragment(activity: Activity) : Fragment() {
 
     private lateinit var Item: RecyclerView
-    private lateinit var auth:FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     private lateinit var recyclerAdapter: historyAdapter
     private lateinit var recyclerLayoutManager: RecyclerView.LayoutManager
-    private lateinit var db:FirebaseFirestore
+    private lateinit var db: FirebaseFirestore
+    private lateinit var refresh: SwipeRefreshLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,16 +47,19 @@ class HistoryFragment(activity:Activity) : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
+
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth= FirebaseAuth.getInstance()
-        db= FirebaseFirestore.getInstance()
-        val itemList=ArrayList<History>()
-        val currentUser=auth.currentUser
-        val email=currentUser!!.email!!
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        val itemList = ArrayList<History>()
+        val currentUser = auth.currentUser
+        val email = currentUser!!.email!!
         Item = view.findViewById(R.id.history_items)
+        refresh = view.findViewById(R.id.swipTorefresh)
         recyclerLayoutManager = LinearLayoutManager(activity)
         recyclerAdapter = historyAdapter(itemList)
         Item.apply {
@@ -58,29 +68,56 @@ class HistoryFragment(activity:Activity) : Fragment() {
             adapter = recyclerAdapter
         }
         db.collection(email).orderBy("date", Query.Direction.DESCENDING).get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
 
-                        val amount=document["amount"] as Double
-                        val s_r_w=document["sent|rec|with"]!!.toString()
-                        val dealer=document["dealer"]!!.toString()
-                        val time=document["date"] as com.google.firebase.Timestamp
-                        val note=document["note"]
-                        val historyItem=History(amount,s_r_w,dealer,time,document.id.toString())
-                        if(note!=null)
-                            historyItem.setNote(note.toString())
-                        itemList.add(historyItem)
-                        val datepart=time.toDate().toString().split("\\s".toRegex())
-                        Log.d("whatt", datepart[1]+"-"+datepart[2]+"-"+datepart[5]+" "+datepart[3])
-                        //Log.d("whatt", x.time.toString())
-                        recyclerAdapter.notifyDataSetChanged()
-                    }
+                    val amount = document["amount"] as Double
+                    val s_r_w = document["sent|rec|with"]!!.toString()
+                    val dealer = document["dealer"]!!.toString()
+                    val time = document["date"] as com.google.firebase.Timestamp
+                    val note = document["note"]
+                    val historyItem = History(amount, s_r_w, dealer, time, document.id.toString())
+                    if (note != null)
+                        historyItem.setNote(note.toString())
+                    itemList.add(historyItem)
+                    val datepart = time.toDate().toString().split("\\s".toRegex())
+                    Log.d(
+                        "whatt",
+                        datepart[1] + "-" + datepart[2] + "-" + datepart[5] + " " + datepart[3]
+                    )
+                    //Log.d("whatt", x.time.toString())
+                    recyclerAdapter.notifyDataSetChanged()
                 }
-                .addOnFailureListener { exception ->
-                    Log.w("test", "Error getting documents: ", exception)
-                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("test", "Error getting documents: ", exception)
+            }
+
+
+        refresh.setOnRefreshListener {
+            Toast.makeText(
+                activity?.baseContext, "Page Refreshed", Toast.LENGTH_SHORT
+            ).show()
+            refresh.isRefreshing = false
+        }
+
+
+
+
 
 
     }
+
+//    private fun refreshApp() {
+//        refresh.setOnRefreshListener {
+//            Toast.makeText(
+//                activity?.baseContext, "Page Refreshed", Toast.LENGTH_SHORT
+//            ).show()
+//            refresh.isRefreshing = false
+//        }
+//
+//
+//    }
+
 
 }
